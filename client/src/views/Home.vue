2d397b3d05b55eecad35488e13c8c573e86972b1
González Serrano <agonzalezs@minsait.com>
2020-05-19 17:10:19 +0200

    changes

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -3,24 +3,31 @@
     <div class="w-full max-w-screen-md">
       <h1 class="p-12 shadow-font text-7xl tracking-tight text-center">Get your Figma tokens</h1>
 
-      <form class="flex justify-center mb-10">
-        <input
-          v-model="authToken"
-          class="flex-1 mr-4 bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-4 px-6 block appearance-none leading-normal border-b-4"
-          type="text"
-          placeholder="Auth user token"
-        />
-        <input
-          v-model="idFile"
-          class="flex-1 mr-4 max-w-sm bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-4 px-6 block appearance-none leading-normal border-b-4"
-          type="text"
-          placeholder="Id File"
-        />
-        <button
-          @click="e => getEvents(e)"
-          class="bg-blue-500 hover:bg-blue-400 text-white font-bold my-1 py-3 px-10 border-b-4 border-blue-700 hover:border-blue-500 rounded"
-        >Get tokens</button>
+      <form class="mb-10">
+        <div class="flex justify-center mb-8">
+          <input-text v-model="authToken" extra-class="flex-1 mr-4" placeholder="Auth token" />
+          <input-text v-model="idFile" extra-class="flex-1 mr-4" placeholder="Id File" />
+        </div>
+
+        <div class="flex justify-center mb-4">
+          <input-text v-model="tokenName" extra-class="mr-4 " placeholder="Token name" />
+          <custom-select :options="typeOptions" v-model="tokenType" />
+
+          <div class="ml-4 flex items-center">
+            <custom-button
+              size="small"
+              type="green"
+              @click="addToken"
+              :disabled="isDisabledAdd"
+            >Add</custom-button>
+          </div>
+
+          <div class="ml-8 pl-8 flex items-center border-l">
+            <custom-button @click="getEvents" :disabled="isDisabledToken">Get tokens</custom-button>
+          </div>
+        </div>
       </form>
+
       <p v-if="loading">Loading...</p>
 
       <div v-if="errorMessage">
@@ -28,6 +35,13 @@
           class="text-red-700 py-3 px-32 text-center bg-red-200 border-red-700 border rounded-lg"
         >{{ errorMessage }}</p>
       </div>
+
+      <ul v-if="tokensSearch">
+        <li
+          v-for="(token, i) in tokensSearch"
+          :key="token.name + i"
+        >{{ `${token.name} - ${token.type}`}}</li>
+      </ul>
 
       <div v-if="tokensData" class="bg-white p-6 border-b-4 border-gray-400 border rounded-lg">
         <vue-json-pretty :showLine="false" :showDoubleQuotes="false" :deep="1" :data="tokensData"></vue-json-pretty>
@@ -39,29 +53,47 @@
 <script>
 import EventTokens from '@/services/EventTokens.js'
 import VueJsonPretty from 'vue-json-pretty'
+import Button from '@/components/Button/Button'
+import Select from '@/components/Select/Select'
+import InputText from '@/components/InputText/InputText'
 
 export default {
   name: 'Home',
   components: {
-    VueJsonPretty
+    VueJsonPretty,
+    CustomButton: Button,
+    CustomSelect: Select,
+    InputText
   },
   data () {
     return {
       authToken: '',
       idFile: '',
+      tokenName: '',
+      tokenType: 0,
       errorMessage: null,
+      tokensSearch: [],
       tokensData: null,
-      loading: false
+      loading: false,
+      typeOptions: [
+        'color',
+        'typography',
+        'space',
+        'radius',
+        'breakpoint',
+        'opacity'
+      ]
     }
   },
   methods: {
     getEvents (e) {
-      e.preventDefault()
       this.loading = true
       this.errorMessage = null
+
       const formData = {
         authToken: this.authToken,
-        idFile: this.idFile
+        idFile: this.idFile,
+        config: this.tokensSearch
       }
 
       EventTokens.getTokens(formData).then(data => {
@@ -74,6 +106,17 @@ export default {
 
         this.loading = false
       })
+    },
+    addToken () {
+      this.tokensSearch.push({ name: this.tokenName, type: this.typeOptions[this.tokenType] })
+    }
+  },
+  computed: {
+    isDisabledToken () {
+      return !(this.tokensSearch.length && this.authToken && this.idFile)
+    },
+    isDisabledAdd () {
+      return !this.tokenName
     }
   }
 }
