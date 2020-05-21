85a392dc0b13646d6a871508bc7e9757247a6c32
González Serrano <agonzalezs@minsait.com>
2020-05-21 13:20:33 +0200

    Changes for deploy

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const bodyParser = require('body-parser')
 var path = require('path')
 const cors = require('cors')
 const app = express()
-const FigmaTokens = require('./src/js/figma-tokens')
+// const FigmaTokens = require('./src/js/figma-tokens')
 
 app.use(bodyParser.json())
 app.use(cors())
-app.use(express.urlencoded({ extended: true }))
+// app.use(express.urlencoded({ extended: true }))
 
 app.post('/test', (req, res) => {
   console.log('get', req.body)
@@ -19,18 +19,18 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(`${__dirname}/src/index.html`))
 })
 
-app.post('/tokens', (req, res) => {
-  const { authToken, idFile, config } = req.body
-  const figmaStyles = new FigmaTokens(authToken, idFile, config)
+// app.post('/tokens', (req, res) => {
+//   const { authToken, idFile, config } = req.body
+//   const figmaStyles = new FigmaTokens(authToken, idFile, config)
 
-  figmaStyles.getTokens().then(data => {
-    const finalTokens = {}
-    Object.entries(data.token).forEach(e => Object.assign(finalTokens, { [e[0]]: e[1] }))
-    res.send(finalTokens)
-  }).catch(() => {
-    res.send({ type: 'error', message: 'invalid data' })
-  })
-})
+//   figmaStyles.getTokens().then(data => {
+//     const finalTokens = {}
+//     Object.entries(data.token).forEach(e => Object.assign(finalTokens, { [e[0]]: e[1] }))
+//     res.send(finalTokens)
+//   }).catch(() => {
+//     res.send({ type: 'error', message: 'invalid data' })
+//   })
+// })
 
 // listen on the port
 app.listen(process.env.PORT || 8888)
