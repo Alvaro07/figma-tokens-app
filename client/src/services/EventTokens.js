32c873090a95516113ae1b25c075c85c9b48138a
González Serrano <agonzalezs@minsait.com>
2020-05-21 13:41:51 +0200

    fix dependency heroku issue

--- client/src/services/EventTokens.js
+++ client/src/services/EventTokens.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 export default {
   async getTokens (formData) {
-    console.log(formData)
     const res = await axios.post('https://figma-tokens-app-server.herokuapp.com/tokens', formData)
+    console.log(res.data)
     return res.data
   }
 }
