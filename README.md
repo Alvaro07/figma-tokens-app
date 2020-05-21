20f9545c330b9a8ae53a11435b4d3d709e1ca48c
González Serrano <agonzalezs@minsait.com>
2020-05-21 14:44:30 +0200

    deploy

--- README.md
+++
@@ -1,5 +0,0 @@
-# figma-tokens-app
-
-An application to visually see the tokens that you can get on your Figma file in a simple way.
-
-![figma tokens app](figma-tokens.PNG)
--- client/src/services/EventTokens.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export default {
-  async getTokens (formData) {
-    const res = await axios.post('https://figma-tokens-app-server.herokuapp.com/tokens', formData)
-    console.log(res.data)
-    return res.data
-  }
-}
