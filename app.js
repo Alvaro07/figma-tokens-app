e210d0ddf1cad5fd9713bc8b896911e47ea73205
González Serrano <agonzalezs@minsait.com>
2020-05-21 13:14:55 +0200

    Changes for deploy

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }))
 // })
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + 'src/index.html'))
+  res.sendFile(path.join(__dirname + '/src/index.html'))
 })
 
 app.post('/tokens', (req, res) => {
