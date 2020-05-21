861cf563769da80128da8a6e2b1cc4a8094de9ab
González Serrano <agonzalezs@minsait.com>
2020-05-21 13:05:13 +0200

    Changes for deploy

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
+
 const FigmaTokens = require('./src/js/figma-tokens')
+
+var allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+}
 
 app.use(bodyParser.json())
 app.use(cors())
+app.use(allowCrossDomain)
 
 app.post('/test', (req, res) => {
   console.log(req.body)
