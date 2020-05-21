04b7e20dc7df66e86d929862ff782ee65f0897c2
González Serrano <agonzalezs@minsait.com>
2020-05-21 11:47:43 +0200

    Add readme

--- app.js
+++ app.js
@@ -9,12 +9,6 @@ app.use(bodyParser.json())
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
-
 app.get('/tokens', (req, res) => {
   console.log('get', req.body)
 })
