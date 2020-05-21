99e0ac96e0508d6661de0e63bc01ba90d3050c94
González Serrano <agonzalezs@minsait.com>
2020-05-21 08:25:27 +0200

    changes for fdeploy

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const FigmaTokens = require('./src/js/figma-tokens')
 
 app.use(bodyParser.json())
 app.use(cors())
-// app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/tokens', (req, res) => {
   console.log('get', req.body)
@@ -26,6 +26,5 @@ app.post('/tokens', (req, res) => {
   })
 })
 
-console.log(FigmaTokens)
 // listen on the port
 app.listen(process.env.PORT || 8080)
