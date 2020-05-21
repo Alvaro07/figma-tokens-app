c6cc58cdeb537ebe364ad5eae2e3a5d7c07a1c63
González Serrano <agonzalezs@minsait.com>
2020-05-21 14:53:37 +0200

    changes for correct deploy

--- client/src/services/EventTokens.js
+++ client/src/services/EventTokens.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 export default {
   async getTokens (formData) {
     const res = await axios.post('https://figma-tokens-app-server.herokuapp.com/tokens', formData)
-    console.log(res.data)
     return res.data
   }
 }
