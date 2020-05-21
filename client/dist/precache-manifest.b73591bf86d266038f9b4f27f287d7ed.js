179068b1476be344bae4830d9d7d33e3c1316033
González Serrano <agonzalezs@minsait.com>
2020-05-21 15:00:28 +0200

    'deploy'

---
+++ client/dist/precache-manifest.b73591bf86d266038f9b4f27f287d7ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e289525c16786c8b20",
+    "url": "/figma-tokens-app/css/app.e1a45294.css"
+  },
+  {
+    "revision": "93343def5b72c279ab5f",
+    "url": "/figma-tokens-app/css/chunk-vendors.055e504d.css"
+  },
+  {
+    "revision": "ed1dfbbfdf0c77445bdaf95dd4692403",
+    "url": "/figma-tokens-app/index.html"
+  },
+  {
+    "revision": "51e289525c16786c8b20",
+    "url": "/figma-tokens-app/js/app.54050789.js"
+  },
+  {
+    "revision": "93343def5b72c279ab5f",
+    "url": "/figma-tokens-app/js/chunk-vendors.4894a6ec.js"
+  },
+  {
+    "revision": "8eff00fb23e5046e8587fb699e79e093",
+    "url": "/figma-tokens-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/figma-tokens-app/robots.txt"
+  }
+]);
